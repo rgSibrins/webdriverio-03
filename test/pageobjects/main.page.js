411fda998f6cc124a869af0331dce0fcdd7685a1
Rodri <rodrigosib2017@gmail.com>
2021-06-03 13:30:40 -0300

    main page header links tests

--- test/pageobjects/main.page.js
+++ test/pageobjects/main.page.js
@@ -1,18 +1,17 @@
 
 class MainPage {
-    
     get spacexLogo () { return $('#logo') }
-    get shopLink () { return $('#navigation-right').$$('.nav-item a') }
+    get shopLink () { return $('#navigation-right > ul > li > a') }
     get hamburgerMenu () {return $('#hamburger')}
-    get hamburgerItems () {return $('.nav-item secondary a')}
-    get leftBtns () {return $('.inner-left-bottom').$$('.btn animate')}
-    get footerCopy () { return $('#footer span')}
-    get footerLinks () { return $('#footer').$$('a')}
-
+    
     async checkHeader (value) {
+        //value from 1 to 6
         await (await $('#navigation > ul > li:nth-child('+`${value}`+') > a')).click();
     }
-
+    async hamburgerItems (value) {
+        //value from 7 to 11
+        await (await $('#menu-navigation > ul > li:nth-child('+`${value}`+') > a')).click();
+    }
     open () {
         return browser.url('https://www.spacex.com/')
     }
--- test/specs/main-automation.js
+++ test/specs/main-automation.js
@@ -4,19 +4,97 @@ describe('Main page', () => {
     beforeAll('Open browser', () => {
         MainPage.open();
     });
-    describe('Checking navbar links', () => {
+    describe('Checking header links', () => {
         it('falcon 9 link should navigates to correct page', async () => {
             await MainPage.checkHeader(1);
             const browserUrl = await browser.getUrl();
             expect(browserUrl).toBe('https://www.spacex.com/vehicles/falcon-9/');
         });
-        it('falcon 9 link should navigates to correct page', async () => {
+        it('falcon heavy link should navigates to correct page', async () => {
             await browser.back();
             await browser.pause(1000);
             await MainPage.checkHeader(2);
             const browserUrl = await browser.getUrl();
             expect(browserUrl).toBe('https://www.spacex.com/vehicles/falcon-heavy/');
-        });    
+        });
+        it('dragon link should navigates to correct page', async () => {
+            await browser.back();
+            await browser.pause(1000);
+            await MainPage.checkHeader(3);
+            const browserUrl = await browser.getUrl();
+            expect(browserUrl).toBe('https://www.spacex.com/vehicles/dragon/');
+        });
+        it('starship link should navigates to correct page', async () => {
+            await browser.back();
+            await browser.pause(1000);
+            await MainPage.checkHeader(4);
+            const browserUrl = await browser.getUrl();
+            expect(browserUrl).toBe('https://www.spacex.com/vehicles/starship/');
+        });
+        it('human spaceflight link should navigates to correct page', async () => {
+            await browser.back();
+            await browser.pause(1000);
+            await MainPage.checkHeader(5);
+            const browserUrl = await browser.getUrl();
+            expect(browserUrl).toBe('https://www.spacex.com/human-spaceflight/');
+        });
+        it('rideshare link should navigates to correct page', async () => {
+            await browser.back();
+            await browser.pause(1000);
+            await MainPage.checkHeader(6);
+            const browserUrl = await browser.getUrl();
+            expect(browserUrl).toBe('https://www.spacex.com/rideshare/');
+        });
+        it('spacex logo should navigates to correct page', async () => {
+            await (await MainPage.spacexLogo).click();
+            await browser.pause(1000);
+            const browserUrl = await browser.getUrl();
+            expect(browserUrl).toBe('https://www.spacex.com/');
+        });
+        it('shop link should navigates to correct page', async () => {
+            await (await MainPage.shopLink).click();
+            await browser.switchWindow('https://shop.spacex.com/');
+            const browserUrl = await browser.getUrl();
+            expect(browserUrl).toBe('https://shop.spacex.com/');
+        });        
+    });
+    describe('Checking hamburger menu', () => {
+        it('mission link navigates to correct page', async () =>{
+            await browser.switchWindow('https://www.spacex.com/');
+            await (await MainPage.hamburgerMenu).click();
+            await MainPage.hamburgerItems(7);
+            await browser.pause(2000);
+            const browserUrl = await browser.getUrl();
+            expect(browserUrl).toBe('https://www.spacex.com/mission/')
+        });
+        it('launches link navigates to correct page', async () =>{
+            await (await MainPage.hamburgerMenu).click();
+            await MainPage.hamburgerItems(8);
+            await browser.pause(2000);
+            const browserUrl = await browser.getUrl();
+            expect(browserUrl).toBe('https://www.spacex.com/launches/')
+        });
+        it('careers link navigates to correct page', async () =>{
+            await (await MainPage.hamburgerMenu).click();
+            await MainPage.hamburgerItems(9);
+            await browser.pause(2000);
+            const browserUrl = await browser.getUrl();
+            expect(browserUrl).toBe('https://www.spacex.com/careers/')
+        });
+        it('updates link navigates to correct page', async () =>{
+            await (await MainPage.hamburgerMenu).click();
+            await MainPage.hamburgerItems(10);
+            await browser.pause(2000);
+            const browserUrl = await browser.getUrl();
+            expect(browserUrl).toBe('https://www.spacex.com/updates/')
+        });
+        it('shop link navigates to correct page', async () =>{
+            await (await MainPage.hamburgerMenu).click();
+            await MainPage.hamburgerItems(11);
+            await browser.switchWindow('https://shop.spacex.com/');
+            const browserUrl = await browser.getUrl();
+            expect(browserUrl).toBe('https://shop.spacex.com/')
+        });
     });
 });
 
